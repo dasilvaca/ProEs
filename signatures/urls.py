e5a52b3bc3e7f796658e3bc3e3eee790ce499fa9
Juan Pablo Garzon Parra <juromerop@unal.edu.co>
2022-01-13 17:33:32 -0500

    Se agregaron las clases y los path de topworst y topbest

--- signatures/urls.py
+++ signatures/urls.py
@@ -6,4 +6,6 @@ from signatures import views
 
 urlpatterns = [
     path("", views.Signatures.as_view(), name="signatures"),
+    path("topworst", views.TopWorst.as_view(), name="topworst"),
+    path("topbest", views.TopBest.as_view(), name="topbest"),
 ]
--- signatures/views.py
+++ signatures/views.py
@@ -4,3 +4,9 @@ from django.views.generic import TemplateView
 # Create your views here.
 class Signatures(TemplateView):
     template_name = "signatures/signatures.html" #"about.html"
+
+class TopWorst(TemplateView):
+    template_name = "signatures/topworst.html" #"about.html"
+
+class TopBest(TemplateView):
+    template_name = "signatures/topbest.html" #"about.html"
