67685fce78dc794072e30448a958f9c2bb8cb2e8
DaSilvaCa <dasilvaca@unal.edu.co>
2022-01-12 22:03:48 -0500

    Initial commit

---
+++ users/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+# from . import views
+
+from users import views
+
+urlpatterns = [
+    path("login", views.Login.as_view(), name="login"),
+]
---
+++ users/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.views.generic import TemplateView
+
+class Login(TemplateView):
+    template_name = "users/login.html" #"about.html"
