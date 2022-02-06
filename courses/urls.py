3a5ec7b0f0b665aa3b2b863b03603fb2d7f46d7d
DanielP <danperezma@unal.edu.co>
2022-02-06 01:25:22 -0500

    pasar daitocos UwU

--- courses/urls.py
+++ courses/urls.py
@@ -7,7 +7,7 @@ from courses import views
 from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
-    path("", views.Courses.as_view(), name = "courses"),
+    path("", views.Courses, name = "courses"),
     path("topworst", views.TopWorst.as_view(), name = "topworst"),
     path("topbest", views.TopBest.as_view(), name = "topbest"),
     path("newcourse", views.NewCourse, name = "newcourse"),
--- courses/views.py
+++ courses/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
@@ -9,8 +9,23 @@ from utils import *
 from bson import ObjectId
 
 # Create your views here.
-class Courses(LoginRequiredMixin, TemplateView):
-    template_name = "courses/courses.html"  # "about.html"
+
+@login_required
+def Courses(request):
+    # Para saber las asignaturas de un profesor
+    profesor = request.user.username
+    asignaturas = []
+    db = connect("proesCol")
+    salida = db.find({"profesor":profesor})
+
+    for x in salida:
+       asignaturas.append(x)
+
+    print(asignaturas)
+
+    # id = str(db.find_one({"asignatura.nombre":"ESTADÍSTICA"},{"_id":1})["_id"])
+    # out = db.find_one({"_id":ObjectId(id)})
+    return render(request, "courses/courses.html", {"contexto": asignaturas})  # "about.html"
 
 
 class TopWorst(LoginRequiredMixin, TemplateView):
@@ -27,12 +42,12 @@ def NewCourse(request):
     profesor = request.user.username
     asignaturas = []
     db = connect("proesCol")
-    salida = db.find({"profesor":profesor})
-    for x in salida:
-        asignaturas.append(x["asignatura"]["nombre"])
+    #salida = db.find({"profesor":profesor})
+    #for x in salida:
+     #   asignaturas.append(x["asignatura"]["nombre"])
 
-    id = str(db.find_one({"asignatura.nombre":"ESTADÍSTICA"},{"_id":1})["_id"])
-    out = db.find_one({"_id":ObjectId(id)})
+    #id = str(db.find_one({"asignatura.nombre":"ESTADÍSTICA"},{"_id":1})["_id"])
+    #out = db.find_one({"_id":ObjectId(id)})
     
     if request.method == 'POST':
         nombre = request.POST["nombre"]
@@ -42,7 +57,7 @@ def NewCourse(request):
         query = {"asignatura":{"nombre":nombre,"min":min,"max":max},"profesor":profesor}
         db = connect("proesCol")
         db.insert(query)
-        #import pdb;pdb.set_trace()
+        return redirect('courses')
     return render(request, "courses/newcourse.html",{"contexto":asignaturas})  # "about.html"
 
 
