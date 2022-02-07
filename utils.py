93041125960c2a6422d1d369bf00be50de9efa37
Brian <bchaparro@unal.edu.co>
2022-02-07 15:30:22 -0500

    Mejorando conexión mongoDB Atlas

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ def connect(col):
     ca = certifi.where()
     conn_str = "mongodb+srv://m001-student:m001-mongodb-basics@sandbox.rtlur.mongodb.net/retryWrites=true&w=majority"
     mongoClient = MongoClient(conn_str, tlsCAFile=ca)
-    db = mongoClient['proesCol'][col]
+    db = mongoClient['proesdb'][col]
     '''
     mongoClient = MongoClient("localhost",27017)
     db = mongoClient["proesdb"][col]
