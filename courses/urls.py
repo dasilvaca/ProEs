7c1234b1580afc5e63f13430f53fc65cbced3dce
Juan Pablo Garzon Parra <juromerop@unal.edu.co>
2022-02-09 16:34:35 -0500

    tops en proceso

--- courses/urls.py
+++ courses/urls.py
@@ -8,8 +8,8 @@ from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     path("", views.Courses, name = "courses"),
-    path("topworst", views.TopWorst, name = "topworst"),
-    path("topbest", views.TopBest, name = "topbest"),
+    path("topworst/<str:id>/", views.TopWorst, name = "topworst"),
+    path("topbest/<str:id>/", views.TopBest, name = "topbest"),
     path("newcourse", views.NewCourse, name = "newcourse"),
     path("spreadsheet/<str:id>/", views.Spreadsheet, name = "spreadsheet"),
     path("delete_course/<str:id>/", views.delete_course, name = "delete_course"),
