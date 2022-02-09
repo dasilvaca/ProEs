4ddf2a168ae6f3e686810d9ea1d572d8d065f738
DanielP <danperezma@unal.edu.co>
2022-02-09 04:40:17 -0500

    Comienzo de generacion de reportes

--- ProEs/settings.py
+++ ProEs/settings.py
@@ -129,6 +129,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+MEDIA_URL = '/media/'
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
--- ProEs/views.py
+++ ProEs/views.py
@@ -1,20 +1,90 @@
 # Create your views here.
+import json
 from django.views.generic import TemplateView
+from django.views import View
 from django.urls import *
-from django.shortcuts import render, redirect
+from django.shortcuts import *
+from django.http import HttpResponse
 from django.contrib import messages
-from django.contrib.auth.views import LoginView, LogoutView, \
-    PasswordResetView, PasswordResetConfirmView
+from django.contrib.auth.views import *
 
 #Para conectar con atlas
 from utils import *
 from bson import ObjectId
+import pymongo
+
+#PDfs
+import os
+from django.conf import settings
+from django.http import HttpResponse
+from django.template.loader import get_template
+from xhtml2pdf import pisa
+from django.contrib.staticfiles import finders
+
 
 class Home(TemplateView):
     template_name = 'ProEs/home.html'
 
+def reports(request, id):
+
+    def link_callback(uri, rel):
+        """
+        Convert HTML URIs to absolute system paths so xhtml2pdf can access those
+        resources
+        """
+        result = finders.find(uri)
+        if result:
+            if not isinstance(result, (list, tuple)):
+                result = [result]
+            result = list(os.path.realpath(path) for path in result)
+            path = result[0]
+        else:
+            sUrl = settings.STATIC_URL  # Typically /static/
+            sRoot = settings.STATIC_ROOT  # Typically /home/userX/project_static/
+            mUrl = settings.MEDIA_URL  # Typically /media/
+            mRoot = settings.MEDIA_ROOT  # Typically /home/userX/project_static/media/
+
+            if uri.startswith(mUrl):
+                path = os.path.join(mRoot, uri.replace(mUrl, ""))
+            elif uri.startswith(sUrl):
+                path = os.path.join(sRoot, uri.replace(sUrl, ""))
+            else:
+                return uri
+
+        # make sure that file exists
+        if not os.path.isfile(path):
+            raise Exception(
+                'media URI must start with %s or %s' % (sUrl, mUrl)
+            )
+        return path
+
+    template_path = 'ProEs/reports.html'
+
+    db = connect("proesCol")
+    query = {"_id":ObjectId(id)}
+    salida= db.find(query)[0]
+
+    context = {
+        'icon': '{}{}'.format(settings.BASE_DIR, '/static/common/logos/2.png'),
+        'contexto':salida
+    }
+    # Create a Django response object, and specify content_type as pdf
+    response = HttpResponse(content_type='application/pdf')
+    #response['Content-Disposition'] = 'attachment; filename="report.pdf"'
+    # find the template and render it.
+    template = get_template(template_path)
+    html = template.render(context)
+
+    # create a pdf
+    pisa_status = pisa.CreatePDF(
+       html, dest=response, link_callback=link_callback)
+    # if error then show some funy view
+    if pisa_status.err:
+       return HttpResponse('We had some errors <pre>' + html + '</pre>')
+    return response
 
 def LoginStudents(request):
+    print(settings.BASE_DIR)
     if request.method == 'POST':
         salidaArr1 = []
         di = request.POST["di"]
